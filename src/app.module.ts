da61e16c1370071aae6263abe3b4a965668c4486
Oleg Voitiuk <olgv@protonmail.com>
2024-02-19 13:00:07 +0200

    E2E tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import { configService } from './configs';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(configService.getMongoUrl()), UserModule],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: async () => {
+        const nodeEnv = configService.getNodeEnv();
+
+        if (nodeEnv === 'test') {
+          const mongod = await MongoMemoryServer.create();
+
+          return {
+            uri: mongod.getUri(),
+          };
+        }
+
+        return {
+          uri: configService.getMongoUrl(),
+        };
+      },
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,70 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
+import { configService } from '../src/configs';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
+    jest.setTimeout(10000);
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe(configService.getValidationOptions()),
+    );
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('register -> login -> logout', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/users/register')
+      .send({
+        email: 'example@gmail.com',
+        password: 'Qwerty123.',
+        firstName: 'John',
+        lastName: 'Doe',
+      })
+      .expect(201)
+      .then((response) => {
+        expect(response.body).toEqual({
+          _id: expect.any(String),
+          email: 'example@gmail.com',
+          firstName: 'John',
+          lastName: 'Doe',
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+          token: expect.any(String),
+        });
+      })
+      .then(() =>
+        request(app.getHttpServer())
+          .post('/users/login')
+          .send({
+            email: 'example@gmail.com',
+            password: 'Qwerty123.',
+          })
+          .expect(200)
+          .then((response) => {
+            expect(response.body).toEqual({
+              _id: expect.any(String),
+              email: 'example@gmail.com',
+              firstName: 'John',
+              lastName: 'Doe',
+              createdAt: expect.any(String),
+              updatedAt: expect.any(String),
+              token: expect.any(String),
+            });
+
+            return request(app.getHttpServer())
+              .get('/users/logout')
+              .set('Authorization', `Bearer ${response.body.token}`)
+              .expect(204);
+          }),
+      );
   });
 });
